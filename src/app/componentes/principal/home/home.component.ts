3915f31e8634b9ced5a2d1023cccbf85616f60ef
mike213112 <mikeramirez-14-31@hotmail.com>
2020-05-15 16:06:27 -0600

    six commit

--- src/app/componentes/principal/home/home.component.ts
+++ src/app/componentes/principal/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'analisis-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  Consultar(){
+    this.router.navigate(['/home/usuarios']);
+  }
+
+  Perfil(){
+    this.router.navigate(['/home/perfil']);
   }
 
 }
